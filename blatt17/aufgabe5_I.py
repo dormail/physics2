34883ddef50ac3b1d2ea248fd783fb7903b62271
Matthias Maile <matthias.maile1@gmail.com>
2020-05-22 08:57:43 +0200

    plot korrigiert

--- blatt17/aufgabe5_I.py
+++ blatt17/aufgabe5_I.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def I(t):
-    return 0.0432 + (0.031 - 0.0432) * np.exp(-2 * 0.001 * t/ 3 * 10**4)
+    return 0.072 - 0.032 * np.exp(-2 * 0.001 * t/ 3 * 10**4)
 
 t = np.linspace(0,2, 1000)
 u = t.copy()
